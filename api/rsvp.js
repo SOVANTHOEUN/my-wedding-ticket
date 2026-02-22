4200dcb138ceb7fda152d9a6fff7233ccd090981
sovanthoeun <sovanthoeun88@gmail.com>
2026-02-23 00:38:34 +0900

    Fix RSVP again

--- api/rsvp.js
+++ api/rsvp.js
@@ -160,7 +160,7 @@ async function getSheets() {
   }
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
